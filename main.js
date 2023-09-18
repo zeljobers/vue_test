7486ecf4069973322b35dfba09566c089c0e17a1
root <root@localhost.my.domain>
2023-09-18 14:37:36 +0200

    Components-n-Props

--- main.js
+++ main.js
@@ -1,59 +1,12 @@
 const app = Vue.createApp({
     data () {
         return {
-            product: "ChatGPTvADS",
-            description: "slika jedna ima ovaj kontekst",
-            selectedVariant : 0,
-            url: "https://www.roadmap.sh",
-            inventory : 1,
-            onSale: true,
-            details: ['50% znanje', "30% vestina", "20% da zapamtim mu ime"],
-            varijante: [
-                {id: 1, ime: "Jovan", prezime: "Jovanović", image: "./assets/1.jpg", quantity : 50},
-                {id:2, ime: "Marko", prezime: "Marković", image: "./assets/2.jpg", quantity : 0},
-
-            ],
-            sizes : [
-                50, 70, 90, 100, 120
-            ],
             cart: 0,
-            styleExample : { 'background-color' : "#00FF00" },
-            styleSize : {
-                 'width' : 10+'px', 
-                 'height': 10+'px', 
-                 'background-color': '#344444' 
-            },
-            brand : "Vue Mastery",
+            premium: false,
         }
     },
     methods: {
-        addToCart() {
-            this.cart += 1
-        },
-        updateVariant(index) {
-            this.selectedVariant = index;
-        },
-        changeSizeStyle(size){
-            this.styleSize = {
-                'width' : size+'px', 
-                'height': size+'px', 
-                'background-color': '#344444' 
-            };
-            return this.styleSize;
-        },
-    },
-    computed : {
-        title() {
-
-            return `${this.product} ${this.brand} ${(this.onSale) ? 'is on sale' : ''}`;
-        },
-        image() {
-            return this.varijante[this.selectedVariant].image;
-        },
-        inStock() {
-            return this.varijante[this.selectedVariant].quantity;
-        },
-
+        
     }
 
 })
