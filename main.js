0890f7216f0aa464ca5325410b5f9185d7dd9a1e
root <root@localhost.my.domain>
2023-09-18 10:09:33 +0200

    nesto

---
+++ main.js
@@ -0,0 +1,14 @@
+// const product="ChatGPTvADS"
+
+const app = Vue.createApp({
+    // data : function () {...}
+    // short-hand
+    data () {
+        // options object
+        return {
+            product: "ChatGPTvADS",
+            description: "slika jedna ima ovaj kontekst",
+        }
+    }
+
+})
