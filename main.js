041af8099f72fc6a5f866cf5ed4bdb41aa375917
root <root@localhost.my.domain>
2023-09-18 11:05:25 +0200

    bruh

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
 // const product="ChatGPTvADS"
 
 const app = Vue.createApp({
-    // data : function () {...}
-    // short-hand
     data () {
-        // options object
         return {
             product: "ChatGPTvADS",
             description: "slika jedna ima ovaj kontekst",
             image: "./assets/1.jpg",
             url: "https://www.roadmap.sh",
+            inStock: false,
+            inventory : 1,
         }
     }
 
